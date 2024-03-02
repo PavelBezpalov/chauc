1ba94311012a8f5fb9fea03ac7fbb3dc4c954a26
Pavlo Bezpalov <pavlo.bezpalov@cd.p2h.com>
2024-03-02 22:00:41 +0200

    Add sniping check

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -1,4 +1,14 @@
 class Bid < ApplicationRecord
   belongs_to :bidder
   belongs_to :lot
+
+  after_save :sniping_check
+
+  private
+
+  def sniping_check
+    return if created_at + 5.minutes < lot.end_time
+
+    lot.update(end_time: lot.end_time + 10.minutes)
+  end
 end
